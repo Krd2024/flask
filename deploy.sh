7d0f5f5ebbeae666cbf290dee04b2ef43fc4f9a0
Krd2024 <polzov.ya@ya.ru>
2024-10-24 22:00:46 +0300

    d15

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,15 @@ docker stop flask_web_1 || true
 # Удалить текущий контейнер
 docker rm flask_web_1 || true
 
-# Пересобрать образ
-docker-compose up -d --build
+docker-compose down
 
+# Удаляем неиспользуемые образы
+# docker image prune -f
+
+# # Запускаем проект
+# docker-compose up -d --build
+docker-compose up -d --build && docker image prune -f
+
+# Удаляем неиспользуемые образы
+docker image prune -f
 echo "Flask приложение обновлено и на http://localhost:5000"
